163467dd52f77d1bc0ddcbf0a98171dfad77db84
nattyo1226 <132973057+nattyo1226@users.noreply.github.com>
2025-05-30 17:57:18 +0900

    feat: for julia

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
     outputs = { ... }: {
         templates = {
+            julia = {
+                description = "A minimal julia project";
+                path = ./julia;
+            };
             typst = {
                 description = "A minimal typst project";
                 path = ./typst;
