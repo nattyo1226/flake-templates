9ebe5708c68c392c8e3da2e75af16bdef1ebbdd9
nattyo1226 <132973057+nattyo1226@users.noreply.github.com>
2026-01-09 17:12:01 +0900

    🐛 Fix links

--- rust/.sideflake/flake.nix
+++ rust/.sideflake/flake.nix
@@ -1,6 +1,6 @@
 {
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-25.11";
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
         flake-parts.url = "github:hercules-ci/flake-parts";
         rust-overlay.url = "github:oxalica/rust-overlay";
     };
