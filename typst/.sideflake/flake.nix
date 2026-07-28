567e51a99ba9cce0894b229178fe0030d9adf005
nattyo1226 <132973057+nattyo1226@users.noreply.github.com>
2026-07-28 13:45:05 +0900

    ✅ add typst lsp and formatter

--- typst/.sideflake/flake.nix
+++ typst/.sideflake/flake.nix
@@ -1,30 +1,37 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
-        flake-parts.url = "github:hercules-ci/flake-parts";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
 
-    outputs = inputs@{
-        nixpkgs,
-        flake-parts,
-        ...
-    }: flake-parts.lib.mkFlake { inherit inputs; } {
-        systems = nixpkgs.lib.platforms.all;
-        perSystem = { pkgs, ... }: let
-            fonts = with pkgs; [
-                source-han-serif
-                plemoljp
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.platforms.all;
+      perSystem =
+        { pkgs, ... }:
+        let
+          fonts = with pkgs; [
+            source-han-serif
+            plemoljp
+          ];
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              tinymist
+              typst
+              typstyle
             ];
-        in {
-            devShells.default = pkgs.mkShell {
-                buildInputs = with pkgs; [
-                    typst
-                ];
 
-                env = {
-                    TYPST_FONT_PATHS = builtins.concatStringsSep ":" fonts;
-                };
+            env = {
+              TYPST_FONT_PATHS = builtins.concatStringsSep ":" fonts;
             };
+          };
         };
     };
 }
