95560f07cdfb5a4cc10e63c3ed9f5ba9ef59c517
nattyo1226 <132973057+nattyo1226@users.noreply.github.com>
2026-08-01 19:04:59 +0900

    ✅ add rust-analyzer to toolchains

--- rust/.sideflake/flake.nix
+++ rust/.sideflake/flake.nix
@@ -1,37 +1,43 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
-        flake-parts.url = "github:hercules-ci/flake-parts";
-        rust-overlay.url = "github:oxalica/rust-overlay";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
-    outputs = inputs@{
-        nixpkgs,
-        flake-parts,
-        rust-overlay,
-        ...
-    }: flake-parts.lib.mkFlake { inherit inputs; } {
-        systems = nixpkgs.lib.platforms.all;
-        perSystem = { system, ... }: let
-            overlays = [ rust-overlay.overlays.default ];
-            pkgs = import nixpkgs {
-                inherit system overlays;
-            };
-            rustToolchain = pkgs.rust-bin.fromRustupToolchain {
-                channel = "stable";
-                components = [
-                    "clippy"
-                    "rustfmt"
-                    "rust-src"
-                ];
-                # targets = [];
-            };
-        in {
-            devShells.default = pkgs.mkShell {
-                buildInputs = [
-                    rustToolchain
-                ];
-            };
+  outputs =
+    inputs@{
+      nixpkgs,
+      flake-parts,
+      rust-overlay,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.platforms.all;
+      perSystem =
+        { system, ... }:
+        let
+          overlays = [ rust-overlay.overlays.default ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          rustToolchain = pkgs.rust-bin.fromRustupToolchain {
+            channel = "stable";
+            components = [
+              "clippy"
+              "rustfmt"
+              "rust-analyzer"
+              "rust-src"
+            ];
+            # targets = [];
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              rustToolchain
+            ];
+          };
         };
     };
 }
